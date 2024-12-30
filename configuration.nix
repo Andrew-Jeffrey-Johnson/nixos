9fd661d723850b59e0179de2a7d3a8f30b776917
Andrew Johnson <andrew.jeffrey.johnson@gmail.com>
2024-12-30 12:46:12 -0800

    accepted nvidia license

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,7 @@
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
-  #nixpkgs.config.nvidia.acceptLicense = true;
+  nixpkgs.config.nvidia.acceptLicense = true;
   
 
   # Enable the KDE Plasma Desktop Environment.
