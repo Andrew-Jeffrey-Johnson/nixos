bd088b37ec1e73e71c3533fa11900a8f1e6d586b
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2025-01-10 00:02:32 -0800

    Re-organized parts

--- configuration.nix
+++ configuration.nix
@@ -31,8 +31,6 @@ in
       "${home-manager}/nixos"
     ];
 
-  # Trusted users
-  nix.settings.trusted-users = [ "root" "@wheel" ];
   users = {
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.andrewj = {
@@ -59,6 +57,9 @@ in
       enable = true;
       userEmail = "andrew.jeffrey.johnson@gmail.com";
       userName = "Andrew-Jeffrey-Johnson";
+      extraConfig = {
+        init.defaultBranch = "master";
+      };
     };
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
@@ -145,21 +146,25 @@ in
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Automatically optimize store every build
-  nix.settings.auto-optimise-store = true;
-
-  # Automatic garbage collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+  nix = {
+    # Trusted users
+    settings.trusted-users = [ "root" "@wheel" ];
+    # Automatic garbage collection
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    settings = {
+      # Automatically optimize store every build
+      auto-optimise-store = true;
+      # Enable nix flakes
+      experimental-features = [ "nix-command" "flakes" ];
+    };
   };
 
   # Automatically upgrade nixOS itself
   system.autoUpgrade.enable  = true;
-
-  # Enable nix flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bluetooth
   hardware.bluetooth.enable = true; # enables support for Bluetooth
@@ -179,56 +184,58 @@ in
   services.xserver.videoDrivers = ["nvidia"];
   #boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
 
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
 
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  hardware.nvidia.prime = {
-    # Option A
-    offload = {
+  hardware = {
+    # Enable OpenGL
+    graphics = {
       enable = true;
-      enableOffloadCmd = true;
     };
-    # Option B
-    #sync.enable = true;
-    intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:1:0:0";
+    nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    nvidia.prime = {
+      # Option A
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+
+      # Option B
+      #sync.enable = true;
+
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
   nixpkgs.config.nvidia.acceptLicense = true;
-  
 
   # Enable the KDE Plasma Desktop Environment.
   #services.xserver.enable = true; # optional
