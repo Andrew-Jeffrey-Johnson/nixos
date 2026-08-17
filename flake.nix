a2586ce8c40489e3225c94ad2f18ed74789934eb
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-08-16 18:39:53 -0700

    Moved home manager for andrew into home-manager

--- flake.nix
+++ flake.nix
@@ -63,8 +63,7 @@
                   useGlobalPkgs = true;
                   extraSpecialArgs = args;
                   users = {
-                    andrew = ./andrew-home-manager.nix;
-                    ave = ./ave-home-manager.nix;
+                    andrew = ./home-manager/andrew.nix;
                   };
                   backupFileExtension = "backup";
                 };
