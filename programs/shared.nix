5042f5d8c0413aa4b6299c1534572b320dd37bfe
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-07 16:09:08 -0700

    officeApps

--- programs/shared.nix
+++ programs/shared.nix
@@ -1,51 +1,19 @@
 { pkgs }: [
-  pkgs.kitty # Terminal
-  pkgs.wget # Get web pages
-  pkgs.wl-clipboard-rs # Terminal clipboard
   pkgs.catppuccin # Many color themes
   pkgs.jdk25 # Java
-  pkgs.goldendict-ng # Multi-language dictionary app
-  pkgs.trash-cli # Command-line trash
-  pkgs.duckdb # Stores commands from cli
-  pkgs.sqlite # Database as a file
   pkgs.blender
-  #pkgs.luanti # FOSS Minecraft
-  #pkgs.waypipe # Enables application forwarding on Wayland similar to ssh -X
-  pkgs.godot # Game development
   pkgs.kdePackages.ksystemlog # Desktop environment event viewer
   pkgs.tor-browser
-  pkgs.prismlauncher # Minecraft
   pkgs.qalculate-qt # Calculator
   pkgs.qbittorrent # Torret
   pkgs.chromium # Open-source portion of chrome
   pkgs.gimp # Rastor Image Editor
   pkgs.audacity # Audio Editor
   pkgs.inkscape # Vector Image Editor
-  pkgs.vlc # Media player
-  pkgs.mpv # Media player
   pkgs.sshfs # Mount
   pkgs.pgadmin4 # Server that hosts a website to view PostgreSQL database
-
-  # Android emulator
-  pkgs.android-tools
-
-  # Screen recording
   pkgs.obs-studio
   pkgs.obs-do
   pkgs.obs-cmd
   pkgs.obs-cli
-
-  # Office Programs
-  pkgs.libreoffice-fresh # Office suite
-  pkgs.hunspell # Spell-checker for libreoffice
-  pkgs.hunspellDicts.en_US-large # English dictionary for hunspell
-
-  # LaTeX Editor
-  pkgs.texstudio
-  pkgs.texliveFull
-  pkgs.poppler # PDF viwer used by texstudio
-
-  # Compression programs
-  pkgs._7zz
-  pkgs.unzip
 ]
