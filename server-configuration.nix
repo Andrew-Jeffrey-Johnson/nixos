74546d79441a4953043c756aea51e82f212da8b0
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-13 13:17:27 -0700

    Changed ip address of nfs

--- server-configuration.nix
+++ server-configuration.nix
@@ -31,8 +31,10 @@
   #NFS Server
   services.nfs.server = {
     enable = true;
+    # You can add more IP addresses for a single entry like this:
+    # /export 10.0.0.183(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
     exports = ''
-      /export 192.168.1.10(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
+      /export 10.0.0.183(rw,fsid=0,no_subtree_check)
     '';
   };
   networking = {
