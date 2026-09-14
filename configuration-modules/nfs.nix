9dd8d13eff35981ecedfdd6a7bc7bfdf445a593c
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-13 23:16:08 -0700

    kuberos attempt 15

--- configuration-modules/nfs.nix
+++ configuration-modules/nfs.nix
@@ -24,14 +24,6 @@ in
     fileSystems."/mnt/luminlapid-server-nfs" = {
       device = "10.0.0.183:/";
       fsType = "nfs";
-      options = [
-        "defaults"
-        "user"
-        "rw"
-        "utf8"
-        "noauto"
-        "umask=000"
-      ];
     };
     # optional, but ensures rpc-statsd is running for on demand mounting
     boot.supportedFilesystems = [ "nfs" ];
