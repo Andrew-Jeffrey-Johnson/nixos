6dbec12f37a15c5c454ded57ab86be0a83a1586e
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-16 21:31:27 -0700

    Attempting nfs 16

--- configuration-modules/nfs.nix
+++ configuration-modules/nfs.nix
@@ -23,16 +23,13 @@ in
     # Options for modules imported in "imports" can be set here.
     boot.initrd.supportedFilesystems = [ "nfs" ];
     boot.initrd.kernelModules = [ "nfs" ];
-    environment.systemPackages = [ pkgs.nfs-utils ];
-    fileSystems."/mnt/luminlapid-server-nfs" = {
-      device = "nfs.luminlapid.com:/andrew";
-      fsType = "nfs";
-      options = [
-        "users"
-      ];
-    };
     # optional, but ensures rpc-statsd is running for on demand mounting
     boot.supportedFilesystems = [ "nfs" ];
+    environment.systemPackages = [ pkgs.nfs-utils ];
+    fileSystems."/mnt/luminlapid-server-nfs" = {
+      device = "10.0.0.183:/andrew";
+      fsType = "nfs";
+    };
     security.wrappers."mount.nfs" = {
       setuid = true;
       owner = "root";
