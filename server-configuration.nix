678a571e405486bce276c7b82fce7410de2ad821
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-16 21:19:48 -0700

    Attempting nfs 16

--- server-configuration.nix
+++ server-configuration.nix
@@ -50,7 +50,7 @@
     # You can add more IP addresses for a single entry like this:
     # /export 10.0.0.183(rw,fsid=0,no_subtree_check) 192.168.1.15(rw,fsid=0,no_subtree_check)
     exports = ''
-      /export/andrew 192.168.2.1/24(rw,fsid=0,no_subtree_check)
+      /export/andrew *(rw,fsid=0,no_subtree_check)
     '';
   };
   # Authentication for NFS server
