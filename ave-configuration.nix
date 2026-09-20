fc2dc5582c62b25932ef2d2245c1f5839d6c0d7f
Andrew-Jeffrey-Johnson <andrew.jeffrey.johnson@gmail.com>
2026-09-20 15:33:19 -0700

    Synced andrew and ave

--- ave-configuration.nix
+++ ave-configuration.nix
@@ -131,7 +131,7 @@
     enable = true;
     enableSSHSupport = true;
   };
-  services.dbus.packages = [ pkgs.gcr ];
+  services.dbus.packages = [ pkgs.gcr_4 ];
 
   security.sudo-rs = {
     enable = true;
